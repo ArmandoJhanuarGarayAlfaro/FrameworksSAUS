0a835391c705ed4dd93506468368780359488e1c
ArmandoJhanuarGarayAlfaro <89482389+ArmandoJhanuarGarayAlfaro@users.noreply.github.com>
2023-05-31 22:00:52 -0600

    Delete comedorSAUS directory

--- comedorSAUS/usuarios/token.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-import six
-
-
-class GeneradorToken(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return (six.text_type(user.id) + six.text_type(timestamp)) + six.text_type(user.is_active)
-
-token_activacion = GeneradorToken()
